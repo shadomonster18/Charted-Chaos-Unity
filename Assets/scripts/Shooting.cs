64d7784ee798fb8f1540adb206410f6ff66ef148
shadomonster18 <161454201+shadomonster18@users.noreply.github.com>
2025-12-06 22:05:34 +0200

    Refactor shooting logic to support multiple fire inputs

--- Assets/scripts/Shooting.cs
+++ Assets/scripts/Shooting.cs
@@ -6,11 +6,18 @@ public class Shooting : MonoBehaviour
 {
     public float shootTimerSet;
     public float shootTimer;
+    public float numBalls;
+    public bool tripleShot;
 
     public Transform firePoint;
+    public Transform firePoint1;
+    public Transform firePoint2;
     public GameObject bullet;
     public AudioSource source;
     public ScreenShake shake;
+    public GameObject cannonBall;
+    public bool AutoFire;
+    public bool cannon;
 
     public float force = 20;
     // Start is called before the first frame update
@@ -22,25 +29,32 @@ public class Shooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1") && shootTimer <= 0)
+        if (!AutoFire)
         {
-            shootTimer = shootTimerSet;
-            Shoot();
+            if (Input.GetButtonDown("Fire1") && shootTimer <= 0 || Input.GetKeyDown(KeyCode.Space) && shootTimer <= 0)
+            {
+                shootTimer = shootTimerSet;
+                Shoot(1);
+            }
+            shootTimer -= Time.deltaTime;
         }
-        if (Input.GetKeyDown(KeyCode.Space) && shootTimer <= 0)
+        else
         {
-            shootTimer = shootTimerSet;
-            Shoot();
+            if (Input.GetButton("Fire1") && shootTimer <= 0 || Input.GetKeyDown(KeyCode.Space) && shootTimer <= 0)
+            {
+                shootTimer = shootTimerSet;
+                Shoot(1);
+            }
+            shootTimer -= Time.deltaTime;
         }
-        shootTimer -= Time.deltaTime;
     }
-    void Shoot()
+    void Shoot(float spawnNumber)
     {
         shake.StartCoroutine(shake.Shaking());
-
         GameObject bulletObj = Instantiate(bullet, firePoint.position, firePoint.rotation);
+
         Rigidbody2D rb = bulletObj.GetComponent<Rigidbody2D>();
-        rb.AddForce(firePoint.up * force, ForceMode2D.Impulse);
+        rb.AddForce(firePoint.rotation * Vector2.up * force, ForceMode2D.Impulse);
         source.Play();
     }
 }
